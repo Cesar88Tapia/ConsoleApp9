a4302380aaa8e252a5ed0960a212491aac57b1bd
César Tapia <c.tapia@korvi.net>
2018-10-10 16:38:28 -0500

    Finished arrays

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -10,6 +10,19 @@ namespace ConsoleApp9
     {
         static void Main(string[] args)
         {
+            int[] Array = new int[12];
+            int i;
+            string Chain;
+            for (i = 0; i < 12; i++)
+            {
+                Chain = Console.ReadLine();
+                Array[i] = Convert.ToInt32(Chain);
+            }
+            for (i = 0; i < 12; i++)
+            {
+                Console.WriteLine("{0}", Array[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
